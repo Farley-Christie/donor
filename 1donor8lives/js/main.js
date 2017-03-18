be595ab5883b0faa70f1007c92d6099938336d1d
Farley-Christie <r_christie3@fanshaweonline.ca>
2017-03-18 17:15:36 -0400

    Add files via upload
    we need to review the copy for the game, I also need the video file (and to add a way to select the other videos if we include them on the site) and backend. did I miss anything else?

--- 1donor8lives/js/main.js
+++ 1donor8lives/js/main.js
@@ -6,9 +6,14 @@ console.log("main.js is working");
 // --------------------------------------------- Variables ---------------------------------------------
 
 var headerNav= document.querySelector("#topNav"),
-burger = document.querySelector("#burger");
+burger = document.querySelector("#burger"),
+scrollBtns = document.querySelectorAll(".scrollNav");
 
-var factNav = document.querySelectorAll(".factNav");
+var factNav = document.querySelectorAll(".factNav"),
+fact = document.querySelector("#fact");
+
+var imgs = document.querySelectorAll(".imgholder"),
+x = 0;
 
 // --------------------------------------------- Functions ---------------------------------------------
 
@@ -21,18 +26,54 @@ function showMenu(){
 }
 function newFact(e){
 	if(e.target.id=="next"){
+		fact.style.opacity = 0;
 		//next fact
 		console.log("next");
+		fact.style.opacity = 1;
 	} else {
+		fact.style.opacity = 0;
 		//prev fact
 		console.log("previous");
+		fact.style.opacity = 1;
 	}
 }
+
+function scroll(e){
+	if(e.target.innerHTML == "Gallery") {
+		TweenMax.to(window, 1, {scrollTo:{y:"#gallery", offsetY:40}});
+	}else if (e.target.innerHTML == "Video") {
+		TweenMax.to(window, 1, {scrollTo:{y:"#videozone", offsetY:35}});
+	}else if (e.target.innerHTML == "Game") {
+		TweenMax.to(window, 1, {scrollTo:{y:"#gamecon", offsetY:50}});
+	}else if (e.target.innerHTML == "Facts") {
+		TweenMax.to(window, 1, {scrollTo:{y:"#facts", offsetY:55}});
+	}else{
+		TweenMax.to(window, 1, {scrollTo:{y:"#social", offsetY:50}});
+	}
+}
+
+
+function loadimg(){
+	setTimeout(function(){
+		imgs[x].style.opacity = 1;
+		x ++;
+		if (x < imgs.length){
+			loadimg();
+		}
+	}, 150)
+}
+
+
+
+loadimg();
+
 
 // --------------------------------------------- Listeners ---------------------------------------------
 
 factNav[0].addEventListener("click",newFact,false);
 factNav[1].addEventListener("click",newFact,false);
 burger.addEventListener("click",showMenu,false);
-
+for(var i = 0; i < scrollBtns.length; i++){
+	scrollBtns[i].addEventListener("click",scroll,false);
+}
 })();
--- 1donor8lives/js/mazegame.js
+++ 1donor8lives/js/mazegame.js
@@ -12,7 +12,35 @@ var mazeHeight = 500;
 var img = new Image();
 var collision = 0; //ether 1 or 0, used to check if the player is trying to move into a wall
 
+var timer = document.querySelector("#gameTime"),
+score = document.querySelector("#gameScore"),
+time = 120,
+points = 0;
+
+var gamecon = document.querySelector("#game"),
+startScreen = document.querySelector("#gameStart"),
+endScreen = document.querySelector("#gameEnd"),
+start = document.querySelector("#startBtn");
+
+var dpad = document.querySelectorAll(".dpad");
+/*
+var Aup = document.querySelector("#a_up"),
+Adown = document.querySelector("#a_down"),
+Aleft = document.querySelector("#a_left"),
+Aright = document.querySelector("#a_right");
+*/
 //Functions
+function init(){
+	img.src = "images/maze.gif";
+	return setInterval(draw, 10); //redraws the player charecter every 10 miliseconds
+}
+
+function gameStart(){
+	startScreen.classList.add("hide");
+	gamecon.classList.remove("hide");
+	var interval = setInterval(updateTimer, 1000);
+}
+
 function drawRect(x,y,w,h){
 	ctx.beginPath();//canvas function begins a new shape
 	ctx.rect(x,y,w,h);
@@ -25,18 +53,32 @@ function clear(){
 	ctx.drawImage(img,0,0);
 }
 
-function init(){
-	img.src = "images/maze.gif"
-	return setInterval(draw, 10); //redraws the player charecter every 10 miliseconds
+function move(e){
+	key = e.keyCode;
+	mover(key);
 }
 
-function move(e){
-	console.log(e.keyCode);
-	console.log(collision);
-	switch(e.keyCode){//finds whitch key was pressed, runs diferent block of code for each
-		case 37: // left arrow
+function dmove(e){
+	if (e.target.id == "a_left"){
+		key = 65;
+	} else if (e.target.id == "a_up"){
+		key = 87;
+	} else if (e.target.id == "a_right"){
+		key = 68;
+	} else {
+		key = 83;
+	} 
+	mover(key);
+}
+
+function mover(key){
+	//console.log(e.keyCode);
+	//console.log(key);
+	switch(key){//finds whitch key was pressed, runs diferent block of code for each
+		case 65: // left arrow
 			if (x-movex>0){ //if this dosnt bring the player off the left side of the screen
 				x -= movex; //change the position of the player by 5 pixles
+				checkGet();
 				clear();
 				checkCollision();
 				if (collision == 1){
@@ -45,9 +87,10 @@ function move(e){
 				}
 			}
 			break;
-		case 38: //up arrow
+		case 87: //up arrow
 			if (y-movey>0){ //if this dosnt bring the player lower than the bottom of the maze
 				y -= movey; //change the position of the player by 5 pixles
+				checkGet();
 				clear();
 				checkCollision();
 				if (collision == 1){
@@ -56,9 +99,10 @@ function move(e){
 				}
 			}
 			break;
-		case 39: //right arrow
+		case 68: //right arrow
 			if (x+movex<mazeWidth){//if this dosnt bring the player off the right side of the maze
 				x += movex;
+				checkGet();
 				clear();
 				checkCollision();
 				if (collision == 1){
@@ -67,9 +111,10 @@ function move(e){
 				}
 			}
 			break;
-		case 40: //down arrow
+		case 83: //down arrow
 			if (y+movey<mazeHeight){//if this dosnt bring the player higher than the top of the maze
 				y += movey;
+				checkGet();
 				clear();
 				checkCollision();
 				if (collision == 1){
@@ -82,7 +127,7 @@ function move(e){
 }
 
 function checkCollision(){
-	var imgData = ctx.getImageData(x,y,15,15); //get info about the pixles surounding the player character in a 15 px grid
+	var imgData = ctx.getImageData(x,y,10,10); //get info about the pixles surounding the player character in a 15 px grid
 	var pix = imgData.data;
 	for (var i=0; i<pix.length;i+=4){
 		if (pix[i] == 0){ //checkes if the selected pixle is black by reading every 4th hex value
@@ -90,13 +135,89 @@ function checkCollision(){
 		}
 	}
 }
+/*
+function checkGet(){
+	var imgData = ctx.getImageData(x,y,10,10); //get info about the pixles surounding the player character in a 15 px grid
+	var pix = imgData.data;
+	for (var i=0; i<pix.length;i+=4){
+		console.log(pix[i]);
+		if (pix[i] == 3){ //checkes if the selected pixle is gray by reading every 4th hex value
+			points = points + 1;
+			console.log("check confirm");
+			score.innerHTML = points;
+		}
+	}
+}
+*/
+
+function checkGet(){
+	if(x>315 && x<335 && y>290 && y<310 && points==0){
+		points = 1;
+	} else if (x>140 && x<160 && y>190 && y<210 && points==1){
+		points = 2;
+	} else if (x>390 && x<410 && y>350 && y<370 && points==2){
+		points = 3;
+	} else if (x>190 && x<210 && y>420 && y<440 && points==3){
+		points = 4;
+	} else if (x>105 && x<125 && y>140 && y<160 && points==4){
+		points = 5;
+	} else if (x>290 && x<310 && y>190 && y<210 && points==5){
+		points = 6;
+	} else if (x>390 && x<410 && y>290 && y<310 && points==6){
+		points = 7;
+	} else if (x>275 && x<295 && y>110 && y<130 && points==7){
+		points = 8;
+		gameOver();
+	} 
+	score.innerHTML = points;
+}
 
 function draw(){
 	clear();
 	ctx.fillStyle = "#FF0000";
-	drawRect(x,y,15,15);
+	drawRect(x,y,10,10);
+	ctx.fillStyle = "#333333";
+	if (points == 0){
+		drawRect(325, 300, 10, 10);
+	} else if(points == 1){
+		drawRect(150, 200, 10, 10);
+	} else if(points == 2){
+		drawRect(400, 360, 10, 10);
+	} else if(points == 3){
+		drawRect(200, 430, 10, 10);
+	} else if(points == 4){
+		drawRect(115, 150, 10, 10);
+	} else if(points == 5){
+		drawRect(300, 200, 10, 10);
+	} else if(points == 6){
+		drawRect(400, 300, 10, 10);
+	}else if(points == 7){
+		drawRect(285, 120, 10, 10);
+	}
+	
+}
+
+function updateTimer(){
+	if (time > 0){
+		timer.innerHTML = time;
+		time = time-1;
+	} else {
+		gameOver();
+	}
+}
+
+function gameOver(){
+	if (gameOver){
+		//console.log("gameOver");
+		gamecon.classList.add("hide");
+		endScreen.classList.remove("hide");
+	}
 }
 
 init();
 //Listeners
-window.addEventListener('keydown',move,true);
+window.addEventListener("keydown",move,true);
+start.addEventListener("click",gameStart,false);
+for (var i = 0; i < dpad.length; i++) {
+	dpad[i].addEventListener("mousedown",dmove,false);
+};
