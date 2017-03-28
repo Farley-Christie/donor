4c16a2e8f0eb6f736e3e5a95f547e4a75d9ca66c
Farley-Christie <r_christie3@fanshaweonline.ca>
2017-03-28 15:01:09 -0400

    Add files via upload
    changes Gabriel requested yesterday and facts calling from an array

--- 1donor8lives/js/main.js
+++ 1donor8lives/js/main.js
@@ -6,33 +6,129 @@ console.log("main.js is working");
 // --------------------------------------------- Variables ---------------------------------------------
 
 var headerNav= document.querySelector("#topNav"),
-burger = document.querySelector("#burger");
+burger = document.querySelector("#burger"),
+burgIcon = document.querySelector("#burger img"),
+reg = document.querySelector("#menulink"),
+scrollBtns = document.querySelectorAll(".scrollNav"),
+splash = document.querySelector("#splash");
 
-var factNav = document.querySelectorAll(".factNav");
+var playBtn = document.querySelector("#vidStarter"),
+video = document.querySelector("video");
+
+var factNav = document.querySelectorAll(".factNav"),
+factholder = document.querySelector("#factholder"),
+//fact = document.querySelectorAll(".fact");
+fact = [
+	"<span class='hashred'>By registering consent for organ and tissue donation, you give hope to the thousands of Ontarians waiting for a transplant.</span> Individuals on the transplant wait list are suffering from organ failure and without the generous gift of life from an organ donor, they will die.Tissue donors can also enhance the lives of recovering burn victims, help restore sight, and allow people to walk again. Transplants not only save lives, they return recipients to productive lives","<span class='hashred'>place holder,</span> they will die. Tissue donors can also enhance the lives of recovering burn victims, help restore sight, and allow people to walk again. Transplants not only save lives, they return recipients to productive lives","<span class='hashred'>place holder 2 burn victims, help restore sight, and allow people to walk</span> Individuals on the transplant wait list are suffering from organ failure and without the generous gift of life from an organ donor, they will die. Tissue donors can also enhance the again. Transplants not only save lives, they return recipients to productive lives"
+];
+
+var imgs = document.querySelectorAll(".imgholder"),
+x = 0,
+factnum = 0;
 
 // --------------------------------------------- Functions ---------------------------------------------
 
 function showMenu(){
 	if(headerNav.style.display=="block"){
 		headerNav.style.display="none";
+		reg.style.top="3.4em";
+		burgIcon.src="images/burger.png";
 	}else{
 		headerNav.style.display="block";
+		reg.style.top="12em";
+		burgIcon.src="images/ex.png";
 	}
 }
+
+function startVideo(){
+	playBtn.classList.add("hide");
+	video.classList.remove("hide");
+	video.play();
+}
+
 function newFact(e){
+	//console.log(fact[factnum]);
 	if(e.target.id=="next"){
-		//next fact
-		console.log("next");
+		//TweenMax.to(fact[factnum],0.4, {opacity:0});
+		if(factnum < fact.length-1){
+			factnum++;			
+		} else {
+			factnum = 0;
+		}
+		factholder.innerHTML=fact[factnum];
+		//TweenMax.to(fact[factnum],0.4, {opacity:1});
 	} else {
-		//prev fact
-		console.log("previous");
+		TweenMax.to(fact[factnum],0.4, {opacity:0});
+		
+		if(factnum < 1){
+			factnum = fact.length-1;
+		} else {
+			factnum = factnum-1;
+		}
+		factholder.innerHTML=fact[factnum];
+		TweenMax.to(fact[factnum],0.4, {opacity:1});
 	}
 }
+
+function scroll(e){
+	if(e.target.innerHTML == "Gallery") {
+		TweenMax.to(window, 1, {scrollTo:{y:"#gallery", offsetY:40}});
+	}else if (e.target.innerHTML == "Video") {
+		TweenMax.to(window, 1, {scrollTo:{y:"#videozone", offsetY:35}});
+	}else if (e.target.innerHTML == "Game") {
+		TweenMax.to(window, 1, {scrollTo:{y:"#gamecon", offsetY:50}});
+	}else if (e.target.innerHTML == "Facts") {
+		TweenMax.to(window, 1, {scrollTo:{y:"#facts", offsetY:55}});
+	}else{
+		TweenMax.to(window, 1, {scrollTo:{y:"#social", offsetY:50}});
+	}
+}
+
+function fade(e){
+	e.target.style.opacity="0.8";
+}
+function unfade(e){
+	e.target.style.opacity="1";
+}
+
+function init(){
+	splash.style.height = (window.innerHeight)+"px";
+	splash.style.paddingTop = ((window.innerHeight)/3)+"px";
+	factholder.innerHTML=fact[factnum];
+	if (window.innerWidth > 1270){
+		setTimeout(function(){
+			imgs[x].style.display = "block";
+			imgs[x].style.opacity = 1;
+			x ++;
+			if (x < imgs.length){
+				init();
+			}
+		}, 150)
+	} else {
+		setTimeout(function(){
+			imgs[x+14].style.opacity = 1;
+			imgs[x+14].style.display = "block";
+			x ++;
+			if (x+14 < 20){
+				init();
+			}
+		}, 150)
+	}
+}
+
+init();
 
 // --------------------------------------------- Listeners ---------------------------------------------
 
 factNav[0].addEventListener("click",newFact,false);
 factNav[1].addEventListener("click",newFact,false);
 burger.addEventListener("click",showMenu,false);
-
+for(var i = 0; i < scrollBtns.length; i++){
+	scrollBtns[i].addEventListener("click",scroll,false);
+}
+playBtn.addEventListener("click",startVideo,false);
+for (var i = 0; i < imgs.length; i++) {
+	imgs[i].addEventListener("mouseenter",fade,false);
+	imgs[i].addEventListener("mouseleave",unfade,false);
+};
 })();
--- 1donor8lives/js/mazegame.js
+++ 1donor8lives/js/mazegame.js
@@ -12,7 +12,35 @@ var mazeHeight = 500;
 var img = new Image();
 var collision = 0; //ether 1 or 0, used to check if the player is trying to move into a wall
 
+var timer = document.querySelector("#gameTime"),
+score = document.querySelector("#gameScore"),
+time = 120,
+points = 0;
+
+var gamecon = document.querySelector("#game"),
+startScreen = document.querySelector("#gameStart"),
+endScreen = document.querySelector("#gameEnd"),
+start = document.querySelector("#startBtn");
+
+var dpad = document.querySelectorAll(".dpad");
+/*
+var Aup = document.querySelector("#a_up"),
+Adown = document.querySelector("#a_down"),
+Aleft = document.querySelector("#a_left"),
+Aright = document.querySelector("#a_right");
+*/
 //Functions
+function init(){
+	img.src = "images/maze.gif";
+	return setInterval(draw, 10); //redraws the player charecter every 10 miliseconds
+}
+
+function gameStart(){
+	startScreen.classList.add("hide");
+	gamecon.classList.remove("hide");
+	var interval = setInterval(updateTimer, 1000);
+}
+
 function drawRect(x,y,w,h){
 	ctx.beginPath();//canvas function begins a new shape
 	ctx.rect(x,y,w,h);
@@ -25,18 +53,37 @@ function clear(){
 	ctx.drawImage(img,0,0);
 }
 
-function init(){
-	img.src = "images/maze.gif"
-	return setInterval(draw, 10); //redraws the player charecter every 10 miliseconds
+function move(e){
+	key = e.keyCode;
+	mover(key);
 }
 
-function move(e){
-	console.log(e.keyCode);
-	console.log(collision);
-	switch(e.keyCode){//finds whitch key was pressed, runs diferent block of code for each
-		case 37: // left arrow
+function noZoom(e) {
+  	e.preventDefault();
+  	e.target.click();
+}
+
+function dmove(e){
+	if (e.target.id == "a_left"){
+		key = 65;
+	} else if (e.target.id == "a_up"){
+		key = 87;
+	} else if (e.target.id == "a_right"){
+		key = 68;
+	} else {
+		key = 83;
+	} 
+	mover(key);
+}
+
+function mover(key){
+	//console.log(e.keyCode);
+	//console.log(key);
+	switch(key){//finds whitch key was pressed, runs diferent block of code for each
+		case 65: // left arrow
 			if (x-movex>0){ //if this dosnt bring the player off the left side of the screen
 				x -= movex; //change the position of the player by 5 pixles
+				checkGet();
 				clear();
 				checkCollision();
 				if (collision == 1){
@@ -45,9 +92,10 @@ function move(e){
 				}
 			}
 			break;
-		case 38: //up arrow
+		case 87: //up arrow
 			if (y-movey>0){ //if this dosnt bring the player lower than the bottom of the maze
 				y -= movey; //change the position of the player by 5 pixles
+				checkGet();
 				clear();
 				checkCollision();
 				if (collision == 1){
@@ -56,9 +104,10 @@ function move(e){
 				}
 			}
 			break;
-		case 39: //right arrow
+		case 68: //right arrow
 			if (x+movex<mazeWidth){//if this dosnt bring the player off the right side of the maze
 				x += movex;
+				checkGet();
 				clear();
 				checkCollision();
 				if (collision == 1){
@@ -67,9 +116,10 @@ function move(e){
 				}
 			}
 			break;
-		case 40: //down arrow
+		case 83: //down arrow
 			if (y+movey<mazeHeight){//if this dosnt bring the player higher than the top of the maze
 				y += movey;
+				checkGet();
 				clear();
 				checkCollision();
 				if (collision == 1){
@@ -82,7 +132,7 @@ function move(e){
 }
 
 function checkCollision(){
-	var imgData = ctx.getImageData(x,y,15,15); //get info about the pixles surounding the player character in a 15 px grid
+	var imgData = ctx.getImageData(x,y,10,10); //get info about the pixles surounding the player character in a 15 px grid
 	var pix = imgData.data;
 	for (var i=0; i<pix.length;i+=4){
 		if (pix[i] == 0){ //checkes if the selected pixle is black by reading every 4th hex value
@@ -91,12 +141,74 @@ function checkCollision(){
 	}
 }
 
+function checkGet(){
+	if(x>265 && x<285 && y>290 && y<310 && points==0){
+		points = 1;
+	} else if (x>280 && x<300 && y>190 && y<210 && points==1){
+		points = 2;
+	} else if (x>195 && x<215 && y>150 && y<170 && points==2){
+		points = 3;
+	} else if (x>110 && x<130 && y>265 && y<285 && points==3){
+		points = 4;
+	} else if (x>140 && x<160 && y>410 && y<430 && points==4){
+		points = 5;
+	} else if (x>290 && x<310 && y>390 && y<410 && points==5){
+		points = 6;
+	} else if (x>390 && x<410 && y>360 && y<380 && points==6){
+		points = 7;
+	} else if (x>250 && x<270 && y>65 && y<85 && points==7){
+		points = 8;
+		gameOver();
+	} 
+	score.innerHTML = points;
+}
+
 function draw(){
 	clear();
 	ctx.fillStyle = "#FF0000";
-	drawRect(x,y,15,15);
+	drawRect(x,y,10,10);
+	ctx.fillStyle = "#333333";
+	if (points == 0){
+		drawRect(275, 300, 10, 10);
+	} else if(points == 1){
+		drawRect(290, 200, 10, 10);
+	} else if(points == 2){
+		drawRect(205, 160, 10, 10);
+	} else if(points == 3){
+		drawRect(120, 275, 10, 10);
+	} else if(points == 4){
+		drawRect(150, 420, 10, 10);
+	} else if(points == 5){
+		drawRect(300, 400, 10, 10);
+	} else if(points == 6){
+		drawRect(400, 370, 10, 10);
+	}else if(points == 7){
+		drawRect(260, 75, 10, 10);
+	}
+}
+
+function updateTimer(){
+	if (time > 0){
+		timer.innerHTML = time;
+		time = time-1;
+	} else {
+		gameOver();
+	}
+}
+
+function gameOver(){
+	if (gameOver){
+		//console.log("gameOver");
+		gamecon.classList.add("hide");
+		endScreen.classList.remove("hide");
+	}
 }
 
 init();
 //Listeners
-window.addEventListener('keydown',move,true);
+window.addEventListener("keydown",move,true);
+start.addEventListener("click",gameStart,false);
+for (var i = 0; i < dpad.length; i++) {
+	dpad[i].addEventListener("touchstart", noZoom, false);
+	dpad[i].addEventListener("click",dmove,false);
+};
